a5f7538a2d11347fd9c21999dae817059a402d09
Elias Kristoffer Grønn Moen <ekgmoen@gmail.com>
2025-02-12 14:42:32 +0000

    added global descriptor table skeleton and strlen

--- src/solo_EKGM/src/kernel.c
+++ src/solo_EKGM/src/kernel.c
@@ -1,19 +1,29 @@
 #include "libc/stdint.h"
 #include "libc/stddef.h"
 #include "libc/stdbool.h"
+#include "libc/string.h"
 #include <multiboot2.h>
 
-
-
-struct multiboot_info {
+struct multiboot_info
+{
     uint32_t size;
     uint32_t reserved;
     struct multiboot_tag *first;
 };
 
+int main(uint32_t magic, struct multiboot_info *mb_info_addr)
+{
 
-int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
+    char *hello_world = "Hello World!";
+    size_t len = strlen(hello_world);
+
+    char *video_memory = (char *)0xb8000;
+    // Write to video memory
+    for (size_t i = 0; i < len; i++)
+    {
+        video_memory[i * 2] = hello_world[i];
+        video_memory[i * 2 + 1] = 0x07;
+    }
 
     return 0;
-
 }
